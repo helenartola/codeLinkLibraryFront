c8f4f6c7f38a4bcd6809da32368232173a3bfac1
Helena Artola <116822570+helenartola@users.noreply.github.com>
2024-01-29 18:35:03 +0100

    Hemos añadido icono en blanco del profile

--- src/pages/profilepage/ProfilePage.jsx
+++ src/pages/profilepage/ProfilePage.jsx
@@ -1,35 +1,34 @@
-import React, { useState } from 'react';
-import './ProfilePage.css';
+import { useState } from "react";
+import "./ProfilePage.css";
 //import { Routes, Route } from "react-router-dom";
 //import ListaDePosts from "../../components/listaPosts/ListaDePosts";
 
 const ProfilePage = () => {
   const [userData, setUserData] = useState({
-    username: 'Ejemplo',
-    email: 'ejmplo@mail.com',
+    username: "Ejemplo",
+    email: "ejemplo@mail.com",
   });
 
   return (
     <div className="profile-container">
       <nav className="navegacion-perfil">
-          <button>Historico de Posts</button>
-          <button>Guardado</button>
-          <button>Ajustes</button>
+        <button>Historico de Posts</button>
+        <button>Guardado</button>
+        <button>Ajustes</button>
       </nav>
       <h1>Perfil de Usuario</h1>
       <section>
-      <div className="user-info">
-        <p>Nombre de usuario: {userData.username}</p>
-        <p>Email: {userData.email}</p>
-      </div>
+        <div className="user-info">
+          <p>Nombre de usuario: {userData.username}</p>
+          <p>Email: {userData.email}</p>
+        </div>
       </section>
-      <section className='posts'>{/* LISTADO DE POSTS */}
-      <div>LISTADO POSTS</div>
-    </section>
-      
+      <section className="posts">
+        {/* LISTADO DE POSTS */}
+        <div>LISTADO POSTS</div>
+      </section>
     </div>
   );
 };
 
 export default ProfilePage;
-
