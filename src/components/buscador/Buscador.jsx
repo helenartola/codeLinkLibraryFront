00a9e9d500a504afda5679dca2ba17acbe500605
annalona <anna.ln@hotmail.com>
2024-01-30 15:58:32 +0100

    Revert "Revert "Lupa en buscador""
    This reverts commit 3ea84c91e5a2444f67fc5fc590a7faee9a4d7e63.

--- src/components/buscador/Buscador.jsx
+++ src/components/buscador/Buscador.jsx
@@ -1,52 +1,71 @@
 import "./Buscador.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { searchService } from "../../services";
 import { Link } from "react-router-dom";
 
+// Define el componente Buscador
 const Buscador = () => {
   // Estado para almacenar el término de búsqueda
   const [searchTerm, setSearchTerm] = useState("");
-
   // Estado para almacenar los resultados de la búsqueda
   const [searchResults, setSearchResults] = useState([]);
 
+  // Función de búsqueda asincrónica utilizando useCallback
+  const search = useCallback(async () => {
+    // Ejecutar la función de búsqueda cuando el término de búsqueda no está vacío
+    if (searchTerm.trim() !== "") {
+      // Llamar al servicio de búsqueda y obtener los resultados
+      const data = await searchService(searchTerm);
+
+      // Actualizar el estado con los resultados de la búsqueda
+      setSearchResults(data);
+    } else {
+      // Si el término de búsqueda está vacío, limpiar los resultados
+      setSearchResults([]);
+    }
+  }, [searchTerm]); // Dependencia incluida en el useCallback
+
   // Efecto que se ejecuta cuando el término de búsqueda cambia
   useEffect(() => {
-    // Función de búsqueda asincrónica
-    const search = async () => {
-      // Ejecutar la función de búsqueda cuando el término de búsqueda no está vacío
-      if (searchTerm.trim() !== "") {
-        // Llamar al servicio de búsqueda y obtener los resultados
-        const data = await searchService(searchTerm);
-
-        // Actualizar el estado con los resultados de la búsqueda
-        setSearchResults(data);
-      } else {
-        // Si el término de búsqueda está vacío, limpiar los resultados
-        setSearchResults([]);
-      }
+    // Función interna para llamar a la función de búsqueda
+    const performSearch = async () => {
+      await search();
     };
 
     // Llamar a la función de búsqueda
-    search();
+    performSearch();
 
     // Dependencia del efecto: se ejecutará cada vez que searchTerm cambie
-  }, [searchTerm]);
+  }, [search, searchTerm]);
 
   // Limpiar la búsqueda
   const clearSearchTerm = () => {
     setSearchTerm("");
   };
 
+  // Función para realizar la búsqueda cuando se hace clic en el botón (lupa)
+  const handleSearchClick = () => {
+    // Llama a la función de búsqueda
+    search();
+  };
+
+  // Renderiza el componente Buscador
   return (
-    <div>
+    <div className="buscador-container">
       {/* Input para que el usuario escriba el término de búsqueda */}
-      <input
-        type="text"
-        placeholder="Search..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        {/* Botón con el icono de la lupa */}
+        <button className="search-button" onClick={handleSearchClick}>
+          <img src="lupa-gris.png" alt="Search Icon" />
+        </button>
+      </div>
+      
       <div className="container-buscador">
         {/* Lista de resultados de la búsqueda */}
         <ul className="lista-enlaces-buscador">
@@ -68,4 +87,3 @@ const Buscador = () => {
 };
 
 export default Buscador;
-
