ecf68c03cde1f612a24a718bf4b166f70d756b05
Helena Artola <116822570+helenartola@users.noreply.github.com>
2024-01-30 19:15:54 +0100

    Hemos mejorado el CSS del buscador, del header y el auth

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Auth = () => {
   return (
-    <section>
+    <section className="seccion-botones-acceso">
       <Link to="/login">
         <button className="auth-button">Log in</button>
       </Link>
