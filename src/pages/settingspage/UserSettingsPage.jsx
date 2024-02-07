99fc1567cdb71c18e051a08e47dd3755564c720a
Helena Artola <116822570+helenartola@users.noreply.github.com>
2024-02-07 18:11:50 +0100

    Hemos modificado CSS user settings page

--- src/pages/settingspage/UserSettingsPage.jsx
+++ src/pages/settingspage/UserSettingsPage.jsx
@@ -4,18 +4,23 @@ import { usuarioAjustes } from "../../services";
 import { Link } from "react-router-dom";
 
 const UserSettingsPage = () => {
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [birthDate, setBirthDate] = useState('');
-  const [bio, setBio] = useState('');
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [bio, setBio] = useState("");
   const [error, setError] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-  
-      const userData = await usuarioAjustes(name, lastName, birthDate, bio, token);
+      const userData = await usuarioAjustes(
+        name,
+        lastName,
+        birthDate,
+        bio,
+        token
+      );
 
       if (userData.token) {
         // Lógica para manejar el token obtenido
@@ -28,26 +33,49 @@ const UserSettingsPage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {error && <p className="error-message">{error}</p>}
+    <div className="caja-ajustes-usuario">
+      <form className="formulario-ajustes-usuario" onSubmit={handleSubmit}>
+        {error && <p className="error-message">{error}</p>}
 
-      <label htmlFor="name">Nombre:</label>
-      <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+        <label htmlFor="name">Nombre:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
 
-      <label htmlFor="lastName">Apellido:</label>
-      <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
+        <label htmlFor="lastName">Apellido:</label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
 
-      <label htmlFor="birthDate">Fecha de nacimiento:</label>
-      <input type="date" id="birthDate" value={birthDate} onChange={(e) => setBirthDate(e.target.value)} />
+        <label htmlFor="birthDate">Fecha de nacimiento:</label>
+        <input
+          type="date"
+          id="birthDate"
+          value={birthDate}
+          onChange={(e) => setBirthDate(e.target.value)}
+        />
 
-      <label htmlFor="bio">Bio:</label>
-      <textarea id="bio" value={bio} onChange={(e) => setBio(e.target.value)} />
+        <label htmlFor="bio">Bio:</label>
+        <textarea
+          id="bio"
+          value={bio}
+          onChange={(e) => setBio(e.target.value)}
+        />
 
-      <button type="submit">Aceptar</button>
-      <Link to="/">
-              <button className="return">Volver</button>
-            </Link>
-    </form>
+        <button type="submit">Aceptar</button>
+        <Link to="/">
+          <button className="return">Volver</button>
+        </Link>
+      </form>
+    </div>
   );
 };
 
