b7117818553dd71b8ef135305a092e37902508d0
Cristina García Gómez <elccorreodecristina13@gmail.com>
2024-02-09 21:58:00 +0100

    reconfiguracion guardados en desarrollo

--- src/pages/savedpage/SavedPage.jsx
+++ src/pages/savedpage/SavedPage.jsx
@@ -1,6 +1,7 @@
 // SavedPage.jsx
 import { useEffect, useState } from 'react';
-//import SavedPosts from '../../components/savedpost/SavedPosts';
+import { Link } from 'react-router-dom';
+import "./SavedPage.css"; 
 
 const SavedPage = () => {
   const [savedPosts, setSavedPosts] = useState([]);
@@ -31,19 +32,24 @@ const SavedPage = () => {
   }, []);
 
   return (
-    <div>
+    <div className="saved-posts-container">
       <h2>Tus Posts Guardados</h2>
       {error ? (
         <p>Error: {error}</p>
       ) : savedPosts.length === 0 ? (
         <p>No tienes posts guardados por el momento.</p>
       ) : (
-        <ul>
+        <ul className="saved-posts-list">
           {savedPosts.map((post) => (
-            <li key={post.postId}>{post.title}</li>
+            <li key={post.postId}>
+              <Link to={`/post/${post.postId}`}>{post.title}</Link>
+            </li>
           ))}
         </ul>
       )}
+
+      {/* Enlace a la página SavedPage con la ruta correcta */}
+      <Link to="/saved">Ir a SavedPage</Link>
     </div>
   );
 };
